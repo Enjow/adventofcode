cdf05cadb0e90a82ea9aec8c3e63e4e2271a544e
Ken <kenjoelim@gmail.com>
2020-04-17 00:26:35 +0200

    6b ez

--- src/aoc6b.kt
+++ src/aoc6b.kt
@@ -1,30 +1,39 @@
 import java.io.BufferedReader
 import java.io.FileReader
 
-private const val FILENAME6 = "day6.txt"
+private const val FILENAME6 = "day6b.txt"
 
 fun main(args: Array<String>) {
     var graph = getStringInput(FILENAME6)
     var graphCounter = HashMap<String, Int>()
     for ((child, parent) in graph) {
-        graphCounter.putIfAbsent(child, distanceOfNode(child, graph))
+        if (child == "YOU" || child == "SAN") {
+            println("child in main $child")
+            markPathWay(graph[child]!!, graph, graphCounter)
+        }
     }
-    var sum = 0
+    var sum = 0  
     for ((key, value) in graphCounter) {
-        sum += value
+        println("$key has value $value")
+        if(value==1)
+            sum += value
     }
     print("Answer: $sum")
 }
 
-private fun distanceOfNode(node: String, graph: HashMap<String, String>): Int {
+private fun markPathWay(node: String, graph: HashMap<String, String>, graphCounter: HashMap<String, Int>) {
     if (graph[node] == "COM") {
+        graphCounter.merge(node, 1,Int::plus)
         println("first node found $node ")
-        return 1
     } else {
-        return distanceOfNode(graph[node]!!, graph)
+        var parent = graph[node]
+
+        println("parent in loop $parent")
+        println("node in loop $node")
+        graphCounter.merge(node, 1,Int::plus)
+        markPathWay(graph[node]!!, graph, graphCounter)
     }
 }
-
 
 private fun getStringInput(filename: String): HashMap<String, String> {
     var fileReader = BufferedReader(FileReader(filename))
@@ -34,11 +43,13 @@ private fun getStringInput(filename: String): HashMap<String, String> {
     var graph = HashMap<String, String>()
     while (line != null) {
         var splitLine = line.split(")")
-        var parent = splitLine[0]; var child = splitLine[1]
+        var parent = splitLine[0];
+        var child = splitLine[1]
         if (graph.containsKey(child))
             error("Child node twice in input")
         graph.putIfAbsent(child, parent)
         line = fileReader.readLine()
+//        println("$child")
     }
     return graph
 }
