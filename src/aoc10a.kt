dab4d252dd6e7152dabf39b9b2ada5f1dbaac6d2
Ken <kenjoelim@gmail.com>
2020-04-19 21:45:58 +0200

    10a

--- src/aoc10a.kt
+++ src/aoc10a.kt
@@ -1,5 +1,7 @@
 import java.io.BufferedReader
 import java.io.FileReader
+import java.lang.Integer.max
+import java.lang.Math.abs
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -10,41 +12,52 @@ fun main(args: Array<String>) {
     var gridHash = getInput(FILENAME10)
     var grid = gridHash.toSortedMap(compareBy<Position2D> { it.y }.thenBy { it.x })
 
-    var gridb = HashMap<Position2D, List<Position2D>>()
+    var gridb = HashMap<Position2D, MutableSet<Position2D>>()
 
     for ((currPos, currValue) in grid) {
         if (currValue == '#') {
-            print("currPos: $currPos")
-            for ((endPos, endValue) in grid) {
-                if(endValue=='#' && currPos != endPos)
-                    print("endpos $endPos")
-                    addIfReachable(currPos,endPos,gridb)
-            }
-            println("")
+            addReachableDirections(currPos, grid, gridb)
         }
     }
 
+    var maxVal = gridb.map { it.value.count() }.max()
+    for ((key, value) in gridb) {
+        if (value.count() == maxVal) {
+            println("Position $key has value: ${value.count()}")
+        }
+    }
 
-
+    
 }
 
-fun addIfReachable(start: Position2D, end: Position2D, gridb: HashMap<Position2D, List<Position2D>>) {
-    if (gridb[start].isNullOrEmpty())
-        gridb[start] = listOf(end)
-    else {
-        var endVector = end - start
-        for (pos in gridb[start]!!) {
-            var possibleBlockVector = pos - start
-            if (endVector.divisibleBy(possibleBlockVector)) {
-                println("divisible")
-            } else {
-                gridb.get(start)!! + listOf(end)
-                println("adding end:")
-                println(gridb[start])
-            }
 
+fun addReachableDirections(
+    activePos: Position2D,
+    grid: SortedMap<Position2D, Char>,
+    gridb: HashMap<Position2D, MutableSet<Position2D>>
+) {
+    if (gridb[activePos].isNullOrEmpty()) {
+        gridb.putIfAbsent(activePos, mutableSetOf())
+    }
+    for ((passivePos, passiveValue) in grid) {
+        if (passiveValue == '#' && passivePos != activePos) {
+            var direction = getSmallestVector(passivePos - activePos)
+
+            gridb[activePos]!!.add(direction)
+//            println("adding direction: $direction from activePos: $activePos" +
+//                    "and passivePos: $passivePos")
         }
     }
+}
+
+
+fun getSmallestVector(pos: Position2D): Position2D {
+    for (denom in (max(abs(pos.x), abs(pos.y)) downTo 1)) {
+        if (pos.x % denom == 0 && pos.y % denom == 0) {
+            return Position2D(pos.x / denom, pos.y / denom)
+        }
+    }
+    return pos
 }
 
 private fun printSortedMap(grid: SortedMap<Position2D, Char>) {
@@ -59,24 +72,6 @@ private fun printSortedMap(grid: SortedMap<Position2D, Char>) {
         yprev = key.y
     }
     println("")
-}
-
-private fun printHashMap(grid: HashMap<Position2D, Char>) {
-    var temp = grid.toSortedMap(compareBy<Position2D> { it.y }.thenBy { it.x })
-    var yprev = 0
-    for ((key, value) in temp) {
-        print("$value")
-//        println("$key. and $value")
-//
-        if (key.y - yprev > 0) {
-            println("")
-        }
-        yprev = key.y
-    }
-    println("")
-
-//    var temp2 = temp.entries.groupBy { it.key.y }.map { it.value.joinToString ("")  }.joinToString("\n")
-//    print(temp2)
 }
 
 private fun getInput(filename: String): HashMap<Position2D, Char> {
